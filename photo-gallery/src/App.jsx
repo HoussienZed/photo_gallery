705def8f8a5fbb96099c6b7805a52cdae98d8cee
Houssien Zeineddine <houssienzd@hotmail.com>
2025-03-17 00:01:31 +0200

    optimizing the style

--- photo-gallery/src/App.jsx
+++ photo-gallery/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-/* import "./App.css"; */
+import "./App.css";
 import NavBar from "./components/nav-bar/";
 import ImageSlider from "./components/image-slider";
 
@@ -7,22 +7,26 @@ export default function App() {
   return (
     <BrowserRouter>
       <NavBar />
-      <header>
-        <h1>Wlecome to my Nb 1 Photo Gallery</h1>
-      </header>
-      <Link to="/image-slider/">Go to Image Slider</Link>
-      <Routes>
-        <Route
-          path="/image-slider/"
-          element={
-            <ImageSlider
-              url={"https://picsum.photos/v2/list"}
-              page={"1"}
-              limit={"10"}
-            />
-          }
-        />
-      </Routes>
+      <div className="div-container">
+        <header>
+          <h1>Wlecome to my Nb 1 Photo Gallery</h1>
+        </header>
+        <Link to="/image-slider/" className="show-image-slider">
+          Show to Image Slider
+        </Link>
+        <Routes>
+          <Route
+            path="/image-slider/"
+            element={
+              <ImageSlider
+                url={"https://picsum.photos/v2/list"}
+                page={"1"}
+                limit={"10"}
+              />
+            }
+          />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
